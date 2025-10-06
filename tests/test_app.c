6eabf6bcdacf69a6d35ef7e40dffa03e16acb33d
U-TOUSHEHCO\PDE-04 <PDE-04@PDE-04.Toushehco.net>
2025-10-06 10:12:10 +0330

    week2 complete - added tests and CI

--- tests/test_app.c
+++ tests/test_app.c
@@ -1,14 +1,14 @@
 #include "unity.h"
 #include "app.h"
-
-void test_add_two_numbers(void) {
-    TEST_ASSERT_EQUAL_INT(5, add(2,3));
-}
-
 void setUp(void) {}
 void tearDown(void) {}
+void test_add_two_numbers(void)
+{
+    TEST_ASSERT_EQUAL_INT(5, add(2, 3));
+}
 
-int main(void) {
+int main(void)
+{
     UNITY_BEGIN();
     RUN_TEST(test_add_two_numbers);
     return UNITY_END();
